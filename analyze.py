4750cb2a62fc36281bf3dbb234c66a431be344ca
Taras Glek <taras@glek.net>
2016-12-11 16:07:37 -0800

    final benchmark for blog post

--- analyze.py
+++ analyze.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import json
 import os
 import sys
@@ -7,9 +8,10 @@ def dump_stats(directory, entry):
     filename = "%s/%s" % (directory, entry)
     stats = json.loads(open(filename).read())
     clat = stats["jobs"][0]["write"]["clat"]["percentile"]
-    print "%s,%s,%s" % (entry, clat["50.000000"], clat["99.000000"])
+    print "%s,%s,%s,%s" % (entry, clat["50.000000"], clat["90.000000"], clat["99.000000"])
 
 def main(directory):
+    print "filename,p50,p90,p99"
     dump_stats(directory, "benchmark_before_output.json")
     dump_stats(directory, "benchmark_fill_output.json")
     for entry in sorted(os.listdir(directory)):
